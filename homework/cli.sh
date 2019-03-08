62de224b7175c49ebdf2a926a99d1bdc050ac996
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-03-08 17:28:56 +0100

    use echo instead of using loop p

--- homework/cli.sh
+++ homework/cli.sh
@@ -5,8 +5,14 @@ touch blank
 for p in {1..5}; do echo "Hello"; done >> greetings.txt
 for p in {1..5}; do cp greetings.txt $p.txt ; done
 
-for p in "cat" "dog" "hamster"; do echo $p; done >> pets.txt
+echo "cat" > pets.txt
+echo "dog" >> pets.txt
+echo "hamster" >> pets.txt
+# for p in "cat" "dog" "hamster"; do echo $p; done >> pets.txt
 
-for p in "cat" "ls" "pwd"; do echo $p; done >> commands.txt
+echo "cat" > commands.txt
+echo "ls" >> commands.txt
+echo "pwd" >> commands.txt
+# for p in "cat" "ls" "pwd"; do echo $p; done >> commands.txt
 
 cat pets.txt commands.txt | sort -u > lovelyCommands.txt
